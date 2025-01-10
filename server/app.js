f977c972243854c8b8b3595bd28998175edebd41
ar1elll <109865007+ar1elll@users.noreply.github.com>
2025-01-10 13:34:52 +0200

    works

---
+++ server/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+const app = express();
+
+const port = 3000;
+
+
+function generateModifiedID() {
+    const alphabet = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i)).join('');
+    const id = new Date().getTime().toString(); 
+    let result = '';
+    let i = 0;
+
+    while (i < id.length) {
+        const randomNum = Math.floor(Math.random() * 3) + 1;
+        const numberFromID = parseInt(id.slice(i, i + randomNum), 10) || 0;
+        const letterIndex = numberFromID % alphabet.length;
+        const letter = alphabet[letterIndex];
+        result += letter + id.slice(i + randomNum, i + 2 * randomNum);
+        i += 2 * randomNum;
+    }
+
+    return {
+        originalID: id,
+        modifiedID: result,
+    };
+}
+
+
+app.get('/generate-uniqe-id', (req, res) => {
+    const uniqeId = generateModifiedID();
+    res.json(uniqeId); 
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  
+
+      <div>
+      </div>
+ 
+  
+}
+
+export default App
